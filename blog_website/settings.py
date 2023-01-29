b22460725accd1c6b1e06f94d3ba8222f6c5cf91
Dhruvil Shah <dhruvilshah2001@gmail.com>
2023-01-29 22:25:29 +0530

    debugFalse

--- blog_website/settings.py
+++ blog_website/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d$ke21e0he3cfyfg--eet90_#)*be#3vff8k5kj%vegy4osab4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -127,7 +127,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-
 STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
 MEDIA_ROOT= os.path.join(BASE_DIR,'media')
